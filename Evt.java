decb7954781c78bf456a7b381e3da81142a17f25
Waltzing <loic.waltzing@etu.unistra.fr>
2025-10-10 09:19:47 +0200

    Add constructor and getters/setters to Evt class

--- Evt.java
+++ Evt.java
@@ -1,7 +1,36 @@
 /*classe des évenements: arrivées et départs */
+
+
 public class Evt{
-    public static final int ARRIVEE = 1;
+    public static final int ARRIVEE = 1; 
     public static final int DEPART = 2;
     
-    
+    private double date; //date de l'évènement
+    private int type; //type de l'évènement 
+
+    //constructeur
+    /**
+     * Constructeur d'un évènement
+     * @param date date de l'évènement
+     * @param type type de l'évènement (ARRIVEE ou DEPART)
+     */
+    public Evt(double date, int type){
+        this.date = date;
+        this.type = type;
+    }
+
+    //getters et setters
+    public double getDate() {
+        return date;
+    }
+    public void setDate(double date) {
+        this.date = date;
+    }
+    public int getType() {
+        return type;
+    }
+    public void setType(int type) {
+        this.type = type;
+    }
+
 }
