274457d0b8b2e64d497a063ed914ff3b36550d3c
Waltzing <loic.waltzing@etu.unistra.fr>
2025-10-11 16:31:21 +0200

    Enhance MM1 and Stats classes with simulation logic and result tracking

--- MM1.java
+++ MM1.java
@@ -1,4 +1,130 @@
 /*classe principale */
 public class MM1{
-    
+    private double lambda; //taux d'arrivée
+    private double mu; //taux de service
+    private double dureeSimulation; //durée de la simulation
+    private boolean debug; //mode debug
+
+    private Ech echancier;
+    private Stats stats;
+    private int numClientCourant = 0;
+    private double dateDernierDepart = 0.0;
+    private int nbClientsDansSysteme = 0; //nombre de clients dans le système
+
+    /**
+     * Constructeur de la classe MM1
+     * @param lambda taux d'arrivée
+     * @param mu taux de service  
+     * @param duree durée de simulation
+     * @param debug mode debug
+     */
+    public MM1(double lambda, double mu, double duree, boolean debug) {
+        this.lambda = lambda;
+        this.mu = mu;
+        this.dureeSimulation = duree;
+        this.debug = debug;
+        
+        this.echancier = new Ech();  // Initialise avec arrivée à t=0
+        this.stats = new Stats(debug);
+        this.numClientCourant = 0;
+        this.dateDernierDepart = 0.0;
+        this.nbClientsDansSysteme = 0;
+    }
+
+    /**
+     * Méthode principale du programme
+     * @param args arguments de la ligne de commande : lambda, mu, durée, debug
+     */
+    public static void main(String[] args){
+        //verification et parsing des arguments
+        if (args.length != 4) {
+            System.err.println("Usage: java MM1 <lambda> <mu> <duree> <debug>");
+            System.exit(1); 
+        }
+        double lambda = Double.parseDouble(args[0]);
+        double mu = Double.parseDouble(args[1]);
+        double duree = Double.parseDouble(args[2]);
+        boolean debug = Integer.parseInt(args[3]) == 1; 
+        
+        //creation de l'instance MM1 et lancement de la simulation
+        MM1 simulation = new MM1(lambda, mu, duree, debug);
+        simulation.simuler();
+    }
+
+    /**
+     * Lance la simulation de la file M/M/1
+     * Boucle principale qui traite les événements jusqu'à la fin de la simulation
+     */
+    private void simuler(){
+        //boucle principale
+        while(!echancier.estVide()){
+            Evt evt = echancier.extraction();
+            double dateEvt = evt.getDate();
+
+            // Vérifier si on dépasse la durée de simulation
+            if(dateEvt > dureeSimulation){
+                break;
+            }
+
+            //mettre à jour les statistiques temporelles
+            stats.majStats(dateEvt, nbClientsDansSysteme);
+
+            if(evt.getType() == Evt.ARRIVEE){
+                traiterArrivee(dateEvt);
+            } else if(evt.getType() == Evt.DEPART){
+                traiterDepart(dateEvt);
+            } else {
+                System.err.println("Erreur: type d'évènement inconnu.");
+            }
+        }
+        //affichage des statistiques finales
+        stats.afficherResultatsTheoriques(lambda, mu, dureeSimulation);
+        stats.afficherResultatsSimulation(dureeSimulation);
+    }
+
+    /**
+     * Traite un événement d'arrivée d'un client
+     * @param dateArrivee date de l'arrivée du client
+     */
+    private void traiterArrivee(double dateArrivee) {
+        boolean fileVide = (nbClientsDansSysteme == 0);
+        
+        //statistiques d'arrivee 
+        stats.enregistrerArrivee(dateArrivee, fileVide);
+        
+        //programmer la prochaine arrivée
+        double prochaineArrivee = dateArrivee + Utile.loiExp(lambda);
+        if(prochaineArrivee <= dureeSimulation){
+            echancier.insertion(new Evt(prochaineArrivee, Evt.ARRIVEE));
+        }
+
+        //programmer depart du client courant
+        double dateDepart;
+        if(fileVide){
+            //file vide, le client est servi immédiatement
+            dateDepart = dateArrivee + Utile.loiExp(mu);
+        } else {
+            //file non vide, le client attend la fin du service du client courant
+            dateDepart = dateDernierDepart + Utile.loiExp(mu);
+        }
+
+        echancier.insertion(new Evt(dateDepart, Evt.DEPART));
+        dateDernierDepart = Math.max(dateDernierDepart, dateDepart);
+
+        nbClientsDansSysteme++;
+        numClientCourant++;
+    }
+
+    /**
+     * Traite un événement de départ d'un client
+     * @param dateDepart date du départ du client
+     */
+    private void traiterDepart(double dateDepart) {
+        // Trouver la date d'arrivée correspondante (FIFO)
+        double dateArrivee = stats.getTempsArrivee(stats.getTotalDepart());
+        
+        stats.enregistrerDepart(dateDepart, dateArrivee);
+        nbClientsDansSysteme--;
+    }
+
 }
--- Stats.java
+++ Stats.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Stats{
     private int totalArrivees = 0; //nombre total d'arrivées
-    private int totalDepart =0; //nombre total de départs
+    public int totalDepart =0; //nombre total de départs
     private int compteurSansAttente =0; //nombre de clients n'ayant pas attendu
     private double totalTempsOccupe =0.0; //temps total pendant lequel le serveur est occupé
     private double totalTempsVide =0.0; //temps total pendant lequel le serveur est inoccupé
@@ -12,7 +12,7 @@ public class Stats{
     private double tempsDernierEvt =0.0; //temps du dernier évènement traité
     private double sommeDureesSejour =0.0; //somme des durées de séjour de tous les clients
     private boolean debug;
-    private ArrayList<Double> listeTempsArrivees; //liste des temps d'arrivée des clients
+    public ArrayList<Double> listeTempsArrivees; //liste des temps d'arrivée des clients
 
 
     /**
@@ -30,8 +30,8 @@ public class Stats{
      * @param dateCourante date courante de l'évènement traité
      * @param nbClientsDansSysteme nombre de clients dans le système avant le traitement de l'évènement
      */
-    public void majStats(double dateCoruante, int nbClientsDansSysteme){
-        double diff = dateCoruante - tempsDernierEvt;
+    public void majStats(double dateCourante, int nbClientsDansSysteme){
+        double diff = dateCourante - tempsDernierEvt;
         if(diff < 0){
             System.err.println("Erreur: la date courante est inférieure à la date du dernier évènement.");
             return;
@@ -47,5 +47,88 @@ public class Stats{
         tempsDernierEvt = dateCoruante;
     }
 
-    
+    /**
+     * Enregistre l'arrivée d'un client
+     * @param dateArrivee date d'arrivée du client
+     * @param filVide true si le serveur était libre à l'arrivée, false sinon
+     */
+    public void enregistrerArrivee(double dateArrivee, boolean fileVide){
+        totalArrivees++;
+        listeTempsArrivees.add(dateArrivee);
+        if(fileVide){
+            compteurSansAttente++;
+        }
+        if(debug){
+            System.out.println("Date=" + dateArrivee + " Arrivee client #" + (totalArrivees - 1));
+        }
+    }
+
+    /**
+     * Enregistre le départ d'un client
+     * @param dateDepart date de départ du client
+     * @param dateArrivee date d'arrivée du client
+     */
+    public void enregistrerDepart(double dateDepart, double dateArrivee){
+        totalDepart++;
+        double dureeSejour = dateDepart - dateArrivee;
+        sommeDureesSejour += dureeSejour;
+        if (debug) {
+        System.out.println("Date=" + dateDepart + " Depart client #" + (totalDepart - 1) + " arrive a t=" + dateArrivee);
+        }
+    }
+
+    /**
+     * Affiche les résultats théoriques
+     */
+    public void afficherResultatsTheoriques(double lambda, double mu, double duree) {
+        double ro = lambda / mu;
+        System.out.println("--------------------");
+        System.out.println("RESULTATS THEORIQUES");
+        System.out.println("--------------------");
+        System.out.println("lambda<mu : file stable");
+        System.out.println("ro (lambda/mu) = " + ro);
+        System.out.println("nombre de clients attendus (lambda x duree) = " + (lambda * duree));
+        System.out.println("Prob de service sans attente (1 - ro) = " + (1 - ro));
+        System.out.println("Prob file occupee (ro) = " + ro);
+        System.out.println("Debit (lambda) = " + lambda);
+        System.out.println("Esp nb clients (ro/1-ro) = " + (ro / (1 - ro)));
+        System.out.println("Temps moyen de sejour (1/mu(1-ro)) = " + (1 / (mu * (1 - ro))));
+    }
+
+    /**
+     * Affiche les résultats de simulation
+     */
+    public void afficherResultatsSimulation(double dureeSimulation) {
+        System.out.println("--------------------");
+        System.out.println("RESULTATS SIMULATION");
+        System.out.println("--------------------");
+        System.out.println("Nombre total de clients = " + totalArrivees);
+        System.out.println("Proportion clients sans attente = " + 
+                        (double) compteurSansAttente / totalArrivees);
+        System.out.println("Proportion clients avec attente = " + 
+                        (double) (totalArrivees - compteurSansAttente) / totalArrivees);
+        System.out.println("Debit = " + (double) totalArrivees / dureeSimulation);
+        System.out.println("Nb moyen de clients dans systeme = " + 
+                        aireSousCourbe / dureeSimulation);
+        System.out.println("Temps moyen de sejour = " + 
+                        sommeDureesSejour / totalDepart);
+    }
+
+    /**
+     * Retourne la date d'arrivée d'un client donné (pour respecter l'ordre FIFO)
+     * @param index index du client dans la liste des arrivées
+     * @return date d'arrivée du client
+     */
+    public double getTempsArrivee(int index) {
+        return listeTempsArrivees.get(index);
+    }
+
+    /**
+     * Retourne le nombre total de départs traités
+     * @return nombre total de départs
+     */
+    public int getTotalDepart() {
+        return totalDepart;
+    }
+
 }
